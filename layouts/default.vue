78639ffca098f1893aae14160e5f9bddc01d517c
Berhane-Hiwet La Rose <berhane.larose@gmail.com>
2020-10-05 05:29:01 -0400

    Customise default view

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,14 +2,15 @@
   <v-app dark>
     <v-navigation-drawer
       v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
+      color="secondary"
+      dark
+      temporary
       fixed
       app
     >
       <v-list>
         <v-list-item
-          v-for="(item, i) in items"
+          v-for="(item, i) in sideNavItems"
           :key="i"
           :to="item.to"
           router
@@ -25,65 +26,56 @@
       </v-list>
     </v-navigation-drawer>
     <v-app-bar
-      :clipped-left="clipped"
+      color="primary"
+      dense
+      dark
       fixed
       app
     >
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
+      <router-link
+        to="/"
+        tag="span"
+        style="cursor: pointer"
       >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
+        <v-toolbar-title class="secondary--text hidden-xs-only" v-text="title" />
+      </router-link>
       <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn
+          v-for="item in horizontalNavItems"
+          :key="item.title"
+          :to="item.link"
+          color="primary"
+          depressed
+        >
+          <v-icon
+            class="hidden-sm-only"
+            left
+          >
+            {{ item.icon }}
+          </v-icon>
+          {{ item.title }}
+        </v-btn>
+      </v-toolbar-items>
     </v-app-bar>
     <v-main>
       <v-container>
         <nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-footer
-      :absolute="!fixed"
+      color="primary"
+      dark
       app
     >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
+      <v-col
+        cols="12"
+        class="text-center secondary--text"
+      >
+        <strong>Your Voice Magazine</strong>
+        <span>&copy; {{ new Date().getFullYear() }}</span>
+      </v-col>
     </v-footer>
   </v-app>
 </template>
@@ -92,25 +84,27 @@
 export default {
   data () {
     return {
-      clipped: false,
       drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+      title: 'Your Voice Magazine'
+    }
+  },
+  computed: {
+    sideNavItems () {
+      const items = [
+        // { icon: 'mdi-apps', title: 'Home', to: '/' },
+        { icon: 'mdi-lock-open', title: 'Sign In', link: '/login' }
+      ]
+
+      return items
+    },
+
+    horizontalNavItems () {
+      const items = [
+        // { icon: 'mdi-apps', title: 'Home', to: '/' },
+        { icon: 'mdi-lock-open', title: 'Sign In', link: '/login' }
+      ]
+
+      return items
     }
   }
 }
