a9d0e9016d489834974f433ca171f3bd37ad33bb
Berhane-Hiwet La Rose <berhane.larose@gmail.com>
2020-10-05 04:54:20 -0400

    Customise error layout

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,14 +1,29 @@
 <template>
   <v-app dark>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/">
-      Home page
-    </NuxtLink>
+    <v-container fluid fill-height grid-list-md text-center>
+      <v-layout row wrap align-center justify-center>
+        <v-flex xs12>
+          <v-card height="400" color="transparent" flat>
+            <div class="display-3 mt-5">
+              Page not found.
+            </div>
+            <div class="grey--text lighten-5" mb-4>
+              The page you are trying to get to never existed in this reality,
+              or has migrated to a parallel universe.
+            </div>
+            <div class="paragraph-text mt-5">
+              <p class="mt-5">
+                Try going back to the
+                <NuxtLink to="/">
+                  home page
+                </NuxtLink>
+                and repeating your action.
+              </p>
+            </div>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </v-app>
 </template>
 
