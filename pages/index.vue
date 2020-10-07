d2c1e6969d66a1b9f51c0319d01bcfe4ec3b3fda
Berhane-Hiwet La Rose <berhane.larose@gmail.com>
2020-10-07 06:56:50 -0400

    Query and cache user details + personal greet

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <v-row>
       <v-col>
         <h1 class="secondary--text">
-          Welcome to Your Voice!
+          Welcome to Your Voice, {{ user.firstName }}!
         </h1>
       </v-col>
     </v-row>
@@ -11,8 +11,25 @@
 </template>
 
 <script>
+import GET_CURRENT_USER from '~/apollo/queries/getCurrentUser'
 export default {
   name: 'Home',
+  data: () => ({
+    user: {
+      firstName: ''
+    }
+  }),
+  apollo: {
+    user: {
+      query: GET_CURRENT_USER,
+      update (data) {
+        return data.getCurrentUser
+      },
+      result ({ data }) {
+        this.$store.commit('setUserRole', data.getCurrentUser.role)
+      }
+    }
+  },
   head: {
     title: 'Home'
   }
