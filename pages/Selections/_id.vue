4e662c866c84ab45e7446421787225388d20fc34
Berhane-Hiwet La Rose <berhane.larose@gmail.com>
2020-10-08 00:17:53 -0400

    EYV-43 Create Selection routes

---
+++ pages/Selections/_id.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container text-center class="secondary--text">
+    <v-row>
+      <v-col>
+        <h1>Selection {{ selection }}</h1>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'SelectionDetails',
+  asyncData ({ params }) {
+    return {
+      selection: params.id
+    }
+  },
+  head () {
+    return {
+      title: this.selection ? `Selection ${this.sub} Details` : 'Selection Details'
+    }
+  }
+}
+</script>
---
+++ pages/Selections/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container text-center class="secondary--text">
+    <v-row>
+      <v-col>
+        <h1>Selections Listing</h1>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Selections',
+  head: 'Selections Listing'
+}
+</script>
