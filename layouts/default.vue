c3269124d82b6f05f450b2b6cc6415079d838bdf
Berhane-Hiwet La Rose <berhane.larose@gmail.com>
2020-10-08 00:54:37 -0400

    EVY-49 Add Profile button

--- layouts/default.vue
+++ layouts/default.vue
@@ -23,6 +23,20 @@
             <v-list-item-title v-text="item.title" />
           </v-list-item-content>
         </v-list-item>
+        <v-divider v-if="isAuthenticated" />
+        <v-list-item
+          v-if="isAuthenticated"
+          to="/auth/profile"
+        >
+          <v-list-item-action>
+            <v-icon>
+              mdi-account-box
+            </v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            Profile
+          </v-list-item-content>
+        </v-list-item>
       </v-list>
     </v-navigation-drawer>
     <v-app-bar
