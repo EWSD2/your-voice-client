ae1f2b3921289dbe9865376f8ced936e0aff82c8
Berhane-Hiwet La Rose <berhane.larose@gmail.com>
2020-10-10 19:51:53 -0400

    EYV-52 Put form in card

--- pages/Years/add.vue
+++ pages/Years/add.vue
@@ -9,142 +9,161 @@
         <h1 class="secondary--text">
           Create an Academic Year
         </h1>
-        <v-form
-          ref="form"
-          v-model="isFormValid"
-          lazy-validation
-          @submit.prevent="createYear"
+        <v-card
+          sm="12"
+          md="8"
+          offset-md="8"
+          shaped
+          elevation="8"
         >
-          <v-row>
-            <v-col
-              cols="12"
-              md="8"
-              offset-md="2"
-            >
-              <v-text-field
-                v-model="title"
-                :rules="titleRules"
-                :counter="40"
-                label="Title"
-                clearable
-                required
-              />
-            </v-col>
-            <v-col
-              cols="12"
-              md="8"
-              offset-md="2"
-            >
-              <v-menu
-                v-model="menu"
-                :close-on-content-click="false"
-                :nudge-right="40"
-                transition="scale-transition"
-                offset-y
-                min-width="290px"
+          <v-form
+            ref="form"
+            v-model="isFormValid"
+            lazy-validation
+            @submit.prevent="createYear"
+          >
+            <v-row>
+              <v-col
+                cols="10"
+                md="8"
+                offset="1"
+                offset-md="2"
               >
-                <template v-slot:activator="{ on, attrs }">
-                  <v-text-field
+                <v-text-field
+                  v-model="title"
+                  :rules="titleRules"
+                  :counter="40"
+                  label="Title"
+                  clearable
+                  required
+                />
+              </v-col>
+              <v-col
+                cols="10"
+                md="8"
+                offset="1"
+                offset-md="2"
+              >
+                <v-menu
+                  v-model="menu"
+                  :close-on-content-click="false"
+                  :nudge-right="40"
+                  transition="scale-transition"
+                  offset-y
+                  min-width="290px"
+                >
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-text-field
+                      v-model="startDate"
+                      label="Start Date"
+                      :rules="startDateRules"
+                      readonly
+                      v-bind="attrs"
+                      v-on="on"
+                    />
+                  </template>
+                  <v-date-picker
                     v-model="startDate"
-                    label="Start Date"
-                    :rules="startDateRules"
-                    readonly
-                    v-bind="attrs"
-                    v-on="on"
+                    color="secondary"
+                    @input="menu = false"
                   />
-                </template>
-                <v-date-picker
-                  v-model="startDate"
-                  color="secondary"
-                  @input="menu = false"
-                />
-              </v-menu>
-            </v-col>
-            <v-col
-              cols="12"
-              md="8"
-              offset-md="2"
-            >
-              <v-menu
-                v-model="menu2"
-                :close-on-content-click="false"
-                :nudge-right="40"
-                transition="scale-transition"
-                offset-y
-                min-width="290px"
+                </v-menu>
+              </v-col>
+              <v-col
+                cols="10"
+                md="8"
+                offset="1"
+                offset-md="2"
               >
-                <template v-slot:activator="{ on, attrs }">
-                  <v-text-field
+                <v-menu
+                  v-model="menu2"
+                  :close-on-content-click="false"
+                  :nudge-right="40"
+                  transition="scale-transition"
+                  offset-y
+                  min-width="290px"
+                >
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-text-field
+                      v-model="endDate"
+                      label="End Date"
+                      :rules="[endDateRules2.exists(), endDateRules2.notBeforeStart(startDate)]"
+                      readonly
+                      v-bind="attrs"
+                      v-on="on"
+                    />
+                  </template>
+                  <v-date-picker
                     v-model="endDate"
-                    label="End Date"
-                    :rules="[endDateRules2.exists(), endDateRules2.notBeforeStart(startDate)]"
-                    readonly
-                    v-bind="attrs"
-                    v-on="on"
+                    color="secondary"
+                    @input="menu2 = false"
                   />
-                </template>
-                <v-date-picker
-                  v-model="endDate"
-                  color="secondary"
-                  @input="menu2 = false"
-                />
-              </v-menu>
-            </v-col>
-            <v-col
-              cols="12"
-              md="8"
-              offset-md="2"
-            >
-              <v-menu
-                v-model="menu3"
-                :close-on-content-click="false"
-                :nudge-right="40"
-                transition="scale-transition"
-                offset-y
-                min-width="290px"
+                </v-menu>
+              </v-col>
+              <v-col
+                cols="10"
+                md="8"
+                offset="1"
+                offset-md="2"
               >
-                <template v-slot:activator="{ on, attrs }">
-                  <v-text-field
+                <v-menu
+                  v-model="menu3"
+                  :close-on-content-click="false"
+                  :nudge-right="40"
+                  transition="scale-transition"
+                  offset-y
+                  min-width="290px"
+                >
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-text-field
+                      v-model="submissionClose"
+                      label="Submission Close Date"
+                      readonly
+                      v-bind="attrs"
+                      v-on="on"
+                    />
+                  </template>
+                  <v-date-picker
                     v-model="submissionClose"
-                    label="Submission Close Date"
-                    readonly
-                    v-bind="attrs"
-                    v-on="on"
+                    color="secondary"
+                    @input="menu3 = false"
                   />
-                </template>
-                <v-date-picker
-                  v-model="submissionClose"
-                  color="secondary"
-                  @input="menu3 = false"
-                />
-              </v-menu>
-            </v-col>
-            <v-col
-              cols="12"
-              md="8"
-              offset-md="2"
-            >
-              <v-select
-                v-model="status"
-                :items="statusOptions"
-                label="Status"
-              />
-            </v-col>
-            <v-col
-              cols="12"
-              md="8"
-              offset-md="2"
-            >
-              <v-btn
-                :disabled="!isFormValid"
-                color="accent"
-                type="submit"
+                </v-menu>
+              </v-col>
+              <v-col
+                cols="10"
+                md="8"
+                offset="1"
+                offset-md="2"
               >
-                Create Year
-              </v-btn>
-            </v-col>
-          </v-row>
-        </v-form>
+                <v-select
+                  v-model="status"
+                  :items="statusOptions"
+                  label="Status"
+                />
+              </v-col>
+              <v-col
+                cols="12"
+                md="8"
+                offset-md="2"
+              >
+                <v-spacer />
+                <v-card-actions>
+                  <v-spacer />
+                  <v-btn
+                    class="accent--text"
+                    text
+                    right
+                    :disabled="!isFormValid"
+                    type="submit"
+                  >
+                    Create Year
+                  </v-btn>
+                </v-card-actions>
+              </v-col>
+            </v-row>
+          </v-form>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
