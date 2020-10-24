c22506c46c56136c876e0a5f7a2577855476dc41
Berhane-Hiwet La Rose <berhane.larose@gmail.com>
2020-10-24 03:26:22 -0400

    Add logo to homepage and add elevation to banner

--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,51 @@
 <template>
   <v-container text-center>
-    <v-row>
+    <v-row wrap>
       <v-col
         cols="12"
       >
-        <v-banner
-          two-line
-        >
-          <v-avatar
-            slot="icon"
-            color="accent"
-            size="40"
+        <v-row>
+          <v-img
+            src="/logo.png"
+            height="250"
+            max-height="300"
+            contain
+            class="pb-5"
+          />
+        </v-row>
+        <v-row>
+          <v-banner
+            two-line
+            elevation="8"
           >
-            <v-icon
-              color="white"
-              icon="mdi-human-greeting"
-            >
-              mdi-human-greeting
-            </v-icon>
-          </v-avatar>
-          <p class="headline secondary--text">
-            Welcome to Your Voice, {{ user.firstName }}!
-            Please select an option from the sidebar.
-          </p>
-          <template v-slot:actions>
-            <v-btn
-              text
+            <v-avatar
+              slot="icon"
               color="accent"
-              to="/auth/profile"
+              size="40"
             >
-              Profile
-            </v-btn>
-          </template>
-        </v-banner>
+              <v-icon
+                color="white"
+                icon="mdi-human-greeting"
+              >
+                mdi-human-greeting
+              </v-icon>
+            </v-avatar>
+            <p class="headline secondary--text">
+              Welcome to Your Voice, {{ user.firstName }}!
+              Please select an option from the sidebar.
+            </p>
+            <template v-slot:actions>
+              <v-btn
+                v-if="user.role === 'STUDENT' || user.role === 'DEV'"
+                text
+                color="accent"
+                to="/auth/profile"
+              >
+                Profile
+              </v-btn>
+            </template>
+          </v-banner>
+        </v-row>
       </v-col>
     </v-row>
   </v-container>
